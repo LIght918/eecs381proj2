25b238995a7d8a8ecc3e416280d4b88c0790653c
ZucchiniSky <scottbommarito@gmail.com>
2015-02-12 16:17:29 -0500

    debug out

--- p2_main.cpp
+++ p2_main.cpp
@@ -342,7 +342,6 @@ int main()
                                     Record *record_ptr = new Record(file);
                                     new_library_title.insert(record_ptr);
                                     new_library_id.insert(record_ptr);
-                                    cout << "read record " << *record_ptr << "\n";
                                     num--;
                                 }
                                 if (!(file >> num))
@@ -353,7 +352,6 @@ int main()
                                 {
                                     Collection *collection_ptr = new Collection(file, new_library_title);
                                     new_catalog.insert(collection_ptr);
-                                    cout << "read collection " << *collection_ptr << "\n";
                                     num--;
                                 }
                                 clear_libraries(library_title, library_id);
