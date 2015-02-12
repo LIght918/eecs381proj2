1633de5cf9cec95fd5cdbf99af6c0fad78e78b0a
ZucchiniSky <scottbommarito@gmail.com>
2015-02-12 17:46:39 -0500

    fix remove bounds

--- String.cpp
+++ String.cpp
@@ -224,7 +224,7 @@ The removed characters must be contained within the String.
 Valid values for i and len are the same as for substring. */
 void String::remove(int i, int len)
 {
-    if (i >= length || i + len > length)
+    if (!(i >= 0 && len >= 0 && i <= length && (i + len) <= length))
     {
         throw String_exception("Remove bounds invalid");
     }
